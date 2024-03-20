64dc0b392b3d211a6c1b1682fb26626428a0c6ce
Nikolay Malaksiano <malax@ukr.net>
2024-03-20 12:12:50 +0200

    Empty project

---
+++ src/main/java/com/com/view/View.java
@@ -0,0 +1,14 @@
+package com.com.view;
+
+import javax.swing.*;
+
+public class View {
+
+    public void start(){
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setBounds(500,500,500,500);
+
+    }
+}
